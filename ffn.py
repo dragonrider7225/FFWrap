3a0bf5d4624ad6c38e8f6ec0ffbd5f389140dd0c
Kevin Moonen <moone140@umn.edu>
2018-05-04 15:33:45 -0500

    Added title and author to ffn

--- ffn.py
+++ ffn.py
@@ -32,6 +32,7 @@ class FFNParser(HTMLParser):
         self.__coverLarge = None
         self.__sTitle = None
         self.__cTitle = None
+        self.__author = None
 
     @staticmethod
     def fixSplitAttribute(attrs, attrName):
@@ -61,28 +62,44 @@ class FFNParser(HTMLParser):
         for i in range(mindex, maxdex):
             del attrs[mindex + 1]
         # Fix un-tupled attribute
-        attrs[mindex] = tuple(attrs[mindex])
+        if mindex >= 0:
+            attrs[mindex] = tuple(attrs[mindex])
+        return attrs
+
+    @staticmethod
+    def fixSplitAttributes(attrs):
+        names = []
+        for k, v in attrs:
+            if v != None and v[:2] == "\\'" and v[-1] != "'":
+                names.append(k)
+        for name in names:
+            FFNParser.fixSplitAttribute(attrs, name)
         return attrs
 
     @staticmethod
     def unquoteAttributes(attrs):
         for i in range(len(attrs)):
+            if attrs[i][1] == None:
+                continue
             if attrs[i][1][:2] == "\\'" and attrs[i][1][-2:] == "\\'":
-                attrs[i] = (attrs[i][0], attrs[i][1][2:-2])
+                v = attrs[i][1][2:-2]
+                attrs[i] = (attrs[i][0], v if len(v) else None)
         return attrs
 
     @staticmethod
     def fixDomains(attrs):
         for i in range(len(attrs)):
-            if attrs[i][1][0] == "/" and attrs[i][1][1] != "/":
-                attrs[i] = (attrs[i][0], "//www.fanfiction.net" + attrs[i][1])
+            attr = attrs[i]
+            if attr[1] == None:
+                continue
+            if attr[1][0] == "/" and (len(attr[1]) == 1 or attr[1][1] != "/"):
+                attrs[i] = (attr[0], "//www.fanfiction.net" + attr[1])
         return attrs
 
     @staticmethod
     def fixAttrs(attrs):
         # HTMLParser splits on space regardless of whether that space is part of a class or style string
-        FFNParser.fixSplitAttribute(attrs, "class")
-        FFNParser.fixSplitAttribute(attrs, "style")
+        FFNParser.fixSplitAttributes(attrs)
         # HTMLParser escapes quotes around attributes
         FFNParser.unquoteAttributes(attrs)
         # Files from the current domain can be referenced with just their (UNIX-style) path
@@ -90,24 +107,23 @@ class FFNParser(HTMLParser):
         return attrs
 
     def handle_starttag(self, tag, attrs):
+        attrs = dict(FFNParser.fixAttrs(attrs))
         if self.__title == None and tag.lower() == "title":
             self.__title = 1
             return
         if self.__fandom == None and tag.lower() == "div":
-            if "id" in dict(attrs).keys() and dict(attrs)["id"] == "pre_story_links":
+            if "id" in attrs.keys() and attrs["id"] == "pre_story_links":
                 self.__fandom = "psl"
                 return
         if self.__fandom == "psl" and tag.lower() == "a":
             self.__fandom = "tll"
             return
         if self.__fandom == "tll" and tag.lower() == "a":
-            self.__fandom = [dict(attrs)["href"]]
+            self.__fandom = [attrs["href"]]
             return
         if self.__coverLarge == None and tag.lower() == "img":
             # Build original tag
             # self.__coverLarge = "<img"
-            attrs = FFNParser.fixAttrs(attrs)
-            # Build original tag
             # for k, v in attrs:
             #     self.__coverLarge += " {}=\"{}\"".format(k, v)
             # self.__coverLarge += ">"
@@ -120,8 +136,6 @@ class FFNParser(HTMLParser):
         if self.__coverSmall == None and tag.lower() == "img":
             # Build original tag
             # self.__coverSmall = "<img"
-            attrs = FFNParser.fixAttrs(attrs)
-            # Build original tag
             # for k, v in attrs:
             #     self.__coverSmall += " {}=\"{}\"".format(k, v)
             # self.__coverSmall += ">"
@@ -133,6 +147,10 @@ class FFNParser(HTMLParser):
             return
         if self.__coverSmall != None and tag.lower() == "b":
             self.__sTitle = 1
+            return
+        if "href" in attrs.keys() and attrs["href"].lower().startswith("//www.fanfiction.net/u/"):
+            authorURL = attrs["href"]
+            self.__author = [authorURL]
             return
 
     def handle_endtag(self, tag):
@@ -147,10 +165,8 @@ class FFNParser(HTMLParser):
             self.__fandom.append(data)
             self.__fandom = ["tld"] + self.__fandom
             self.__title = self.__title[:-(len(data) + 4)]
-            print(self.__fandom)
             return
         if self.__sTitle == 1: # data argument contains story title
-            sTitle = data
             # Remove story title from __title field
             self.__title = self.__title[len(data) + 1:]
             # __title is of the form "Chapter [1-9][0-9]*: .+"
@@ -170,16 +186,19 @@ class FFNParser(HTMLParser):
                     chNum = int(s)
                     self.__title = self.__title[:8 + len(s)]
             cTitle = self.__title
-            self.__title = "{}, Chapter {}".format(sTitle, chNum)
+            self.__title = "{}, Chapter {}".format(data, chNum)
             if len(cTitle) > 0:
                 self.__title += ": {}".format(cTitle)
-            print("Data title: {}".format(self.__title))
-            self.__sTitle = 2
+            self.__sTitle = data
+            return
+        if self.__author != None and len(self.__author) == 1:
+            self.__author.append(data)
             return
 
     def getReplacement(self, origin, url):
         self.reset()
         self.feed(origin)
+        print(self.__title)
         ret = HTMLDOM()
         ret.appendToHead(HTMLTag("title", HTMLTextNode(self.__title)))
         style = StyleSheet()
@@ -187,53 +206,73 @@ class FFNParser(HTMLParser):
         style.appendChild(bodyStyle)
         coversStyle = HTMLStyleNode("img", "coverSmall")
         coversStyle.addStyle("position", "relative").addStyle("left", "53px")
-        coversStyle.addStyle("top", "-170px").addStyle("z-index", "1")
+        coversStyle.addStyle("top", "-170px")
         style.appendChild(coversStyle)
         coverlStyle = HTMLStyleNode("img", "coverLarge")
         coverlStyle.addStyle("opacity", "0.5")
+        coverlStyle.addStyle("border-radius", COVER_FRAME_RADIUS)
         style.appendChild(coverlStyle)
-        headerStyle = HTMLStyleNode("div", "storyHeader")
+        headerStyle = HTMLStyleNode("div", "header")
         headerStyle.addStyle("background-color", HEADER_COLOR)
-        headerStyle.addStyle("min-height", "{}px".format(HEADER_HEIGHT))
-        headerStyle.addStyle("max-height", "{}px".format(HEADER_HEIGHT))
+        headerStyle.addStyle("display", HEADER_DISPLAY)
+        headerStyle.addStyle("min-height", HEADER_HEIGHT)
+        headerStyle.addStyle("max-height", HEADER_HEIGHT)
+        headerStyle.addStyle("margin-top", "0px")
+        headerStyle.addStyle("padding-top", HEADER_PADDING_TOP)
+        headerStyle.addStyle("padding-bottom", HEADER_PADDING_BOTTOM)
+        headerStyle.addStyle("padding-left", HEADER_PADDING_LEFT)
         style.appendChild(headerStyle)
         coverFrameStyle = HTMLStyleNode("div", "coverFrame")
-        coverFrameStyle.addStyle("background", COVER_FRAME_COLOR)
-        coverFrameStyle.addStyle("border-radius", "{}px".format(COVER_FRAME_RADIUS))
-        coverFrameStyle.addStyle("padding", "{}px".format(COVER_FRAME_PADDING))
-        coverConStyle = HTMLStyleNode("div", "coverContainer")
-        coverConStyle.addStyle("background-color", BACKGROUND_COLOR)
-        coverConStyle.addStyle("max-width", "{}px".format(COVER_WIDTH))
-        coverConStyle.addStyle("max-height", "{}px".format(COVER_HEIGHT))
-        style.appendChild(coverConStyle)
+        coverFrameStyle.addStyle("background", BACKGROUND_COLOR)
+        coverFrameStyle.addStyle("border-radius", COVER_FRAME_RADIUS)
+        coverFrameStyle.addStyle("max-width", COVER_WIDTH)
+        coverFrameStyle.addStyle("max-height", COVER_HEIGHT)
+        style.appendChild(coverFrameStyle)
+        infoBoxStyle = HTMLStyleNode("div", "infobox")
+        infoBoxStyle.addStyle("padding-left", INFOBOX_PADDING_LEFT)
+        style.appendChild(infoBoxStyle)
+        titleBoxStyle = HTMLStyleNode("div", "titlebox")
+        titleBoxStyle.addStyle("display", TITLEBOX_DISPLAY)
+        titleBoxStyle.addStyle("padding-right", TITLEBOX_PADDING_RIGHT)
+        style.appendChild(titleBoxStyle)
+        authorBoxStyle = HTMLStyleNode("div", "authorbox")
+        authorBoxStyle.addStyle("display", AUTHORBOX_DISPLAY)
+        style.appendChild(authorBoxStyle)
         ret.appendToHead(style)
-        storyHeader = HTMLTag("div", id="storyHeader")
+        storyHeader = HTMLTag("div", id="header")
         coverFrame = HTMLTag("div", id="coverFrame")
-        coverContainer = HTMLTag("div", id="coverContainer")
         coverlReq = Request(self.__coverLarge, headers={"referer": url})
-        coversReq = Request(self.__coverSmall, headers={"referer": url})
-        b64Prefix = "data:image/png;base64, "
-        coverlb64 = b64Prefix + enc(urlopen(coverlReq).read()).decode("utf-8")
-        coversb64 = b64Prefix + enc(urlopen(coversReq).read()).decode("utf-8")
+        coverlb64 = B64DATA_PREFIX + enc(urlopen(coverlReq).read()).decode("utf-8")
         coverlTag = HTMLTag("img", [], src=coverlb64, id="coverLarge")
+        coverFrame.appendChild(coverlTag)
+        coversReq = Request(self.__coverSmall, headers={"referer": url})
+        coversb64 = B64DATA_PREFIX + enc(urlopen(coversReq).read()).decode("utf-8")
         coversTag = HTMLTag("img", [], src=coversb64, id="coverSmall")
-        coverContainer.appendChild(coverlTag)
-        coverContainer.appendChild(coversTag)
-        coverFrame.appendChild(coverContainer)
+        coverFrame.appendChild(coversTag)
         storyHeader.appendChild(coverFrame)
+        infoBox = HTMLTag("div", id="infobox")
+        titleBox = HTMLTag("div", id="titlebox")
+        titleHeading = HTMLTag("h1", HTMLTextNode(self.__sTitle), id="title")
+        titleBox.appendChild(titleHeading)
+        infoBox.appendChild(titleBox)
+        authorBox = HTMLTag("div", id="authorbox")
+        authorHeading = HTMLTag("h2", id="author")
+        authorLink = HTMLTag("a", href=self.__author[0])
+        authorLink.appendChild(HTMLTextNode(self.__author[1]))
+        authorHeading.appendChild(authorLink)
+        authorBox.appendChild(authorHeading)
+        infoBox.appendChild(authorBox)
+        storyHeader.appendChild(infoBox)
         ret.appendToBody(storyHeader)
-        return str(ret)
+        return ret
 
 def testFFN():
     # Test fanfiction.net parser on a story with only one chapter
-    html = loadFFN(12920816, 1)
     with open("ffnTestSingleton.html", "w+") as fout:
-        fout.write(html)
+        fout.write(str(loadFFN(12920816, 1)))
     # Test fanfiction.net parser on a story with multiple unnamed chapters
-    html = loadFFN(12472897, 1)
     with open("ffnTestMulti.html", "w+") as fout:
-        fout.write(html)
+        fout.write(str(loadFFN(12472897, 1)))
     # Test fanfiction.net parser on a story with multiple named chapters
-    html = loadFFN(12919509, 1)
     with open("ffnTestMultiName.html", "w+") as fout:
-        fout.write(html)
+        fout.write(str(loadFFN(12919509, 1)))
